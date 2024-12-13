90881f44f25f5bdab8187f6bcebf1646dc20791c
ryrylleee <153406870+ryrylleee@users.noreply.github.com>
2024-12-14 04:16:15 +0800

    Update RecyclableWaste.java

--- Waste/RecyclableWaste.java
+++ Waste/RecyclableWaste.java
@@ -18,10 +18,10 @@ public class RecyclableWaste extends Waste {
         System.out.println("==============================================================================");
         System.out.println("\t\tThe item \"" + getName() + "\" is recyclable.");
         System.out.println("==============================================================================");
-        System.out.println("\tTip for bottles, cans, and plastic wastes ♻: \nRinse the item before recycling. Remove caps or lids and recycle them separately.\n");
-        System.out.println("\t\tTip for glass waste ♻: \nClean it thoroughly before using it for other purposes. \nIf BROKEN, please wrap it in a paper before disposing it in a garbage bin.\n");
-        System.out.println("\t\tTip for paper waste ♻: \nAvoid shredding paper excessively as it reduces its recyclability. \nEnsure the paper is clean and dry.\n");
-        System.out.println("\t\tTip for metal waste ♻: \nSeparate aluminum and steel items. Use a magnet to identify steel (it sticks).\nFlatten cans to save space when recycling.\n");
+        System.out.println("\tTip for bottles, cans, and plastic wastes: \nRinse the item before recycling. Remove caps or lids and recycle them separately.\n");
+        System.out.println("\t\tTip for glass waste: \nClean it thoroughly before using it for other purposes. \nIf BROKEN, please wrap it in a paper before disposing it in a garbage bin.\n");
+        System.out.println("\t\tTip for paper waste: \nAvoid shredding paper excessively as it reduces its recyclability. \nEnsure the paper is clean and dry.\n");
+        System.out.println("\t\tTip for metal waste: \nSeparate aluminum and steel items. Use a magnet to identify steel (it sticks).\nFlatten cans to save space when recycling.\n");
         System.out.println("==============================================================================");
         return null;
     }
