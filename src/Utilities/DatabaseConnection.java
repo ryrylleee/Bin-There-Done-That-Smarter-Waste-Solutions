8b667415da7212188830a9b5ee5a3ac0c59fc469
ryrylleee <delosreyeskarylle@gmail.com>
2024-12-19 21:20:31 +0800

    updated database connection

--- src/Utilities/DatabaseConnection.java
+++ src/Utilities/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/delosreyes_bintheredonethat"; 
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/bintheredonethat"; 
     private static final String USER = "root";      
     private static final String PASSWORD = "password"; 
 
